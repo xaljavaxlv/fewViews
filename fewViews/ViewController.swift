3950768e4d0cc8424b83edab8695486ec3541b4f
xlv <xaljavamail@gmail.com>
2021-10-21 18:55:10 +0800

    Initial Commit

---
+++ fewViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fewViews
+//
+//  Created by Vlad Zavada on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
